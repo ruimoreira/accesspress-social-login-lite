383e613cfb60999d8c35071f76a7fee06706d240
Rui Moreira <ruimoreira29@gmail.com>
2021-11-19 16:32:19 +0000

    - According to docs is_verified is now not a valid field and it breaks login

--- inc/frontend/login_check.php
+++ inc/frontend/login_check.php
@@ -3,19 +3,19 @@
 defined( 'ABSPATH' ) or die( "No script kiddies please!" );
 
 if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
-    
+
     class APSL_Lite_Login_Check_Class {
         //constructor
         function __construct() {
             if( isset( $_GET['apsl_login_id'] ) ) {
                 if( isset( $_REQUEST['state'] ) ) {
                     parse_str( base64_decode( $_REQUEST['state'] ), $state_vars );
-                    
+
                     if( isset( $state_vars['redirect_to'] ) ) {
                         $_GET['redirect_to'] = $_REQUEST['redirect_to'] = $state_vars['redirect_to'];
                     }
                 }
-                
+
                 $exploder = explode( '_', $_GET['apsl_login_id'] );
                 switch( $exploder[0] ) {
                     case 'facebook':
@@ -47,7 +47,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             if( isset( $result->status ) == 'SUCCESS' ) {
                 $row = $this->getUserByMail( $result->email );
                 $options = get_option( APSL_SETTINGS );
-                
+
                 if( !$row ) {
                     $this->creatUser( $result->username, $result->email );
                     $row = $this->getUserByMail( $result->email );
@@ -64,7 +64,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                 $this->loginUser( $row->ID );
             }
         }
-        
+
         function facebookLogin() {
             $request = $_REQUEST;
             $site = $this->siteUrl();
@@ -75,10 +75,10 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             $action = $exploder[1];
             $options = get_option( APSL_SETTINGS );
             $config = array('app_id' => $options['apsl_facebook_settings']['apsl_facebook_app_id'], 'app_secret' => $options['apsl_facebook_settings']['apsl_facebook_app_secret'], 'default_graph_version' => 'v2.4');
-            
+
             include( APSL_PLUGIN_DIR . 'facebook/autoload.php' );
             $fb = new Facebook\Facebook( $config );
-            
+
             $encoded_url = isset( $_GET['redirect_to'] ) ? $_GET['redirect_to'] : '';
             if( isset( $encoded_url ) && $encoded_url != '' ) {
                 $callback = $callBackUrl . 'apsl_login_id' . '=facebook_check&redirect_to=' . $encoded_url;
@@ -86,7 +86,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             else {
                 $callback = $callBackUrl . 'apsl_login_id' . '=facebook_check';
             }
-            
+
             if( $action == 'login' ) {
                 // Well looks like we are a fresh dude, login to Facebook!
                 $helper = $fb->getRedirectLoginHelper();
@@ -117,14 +117,14 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                         echo 'Facebook SDK returned an error: ' . $e->getMessage();
                         exit;
                     }
-                    
+
                     if( isset( $accessToken ) ) {
                         // Logged in!
                         $_SESSION['facebook_access_token'] = (string)$accessToken;
                         $fb->setDefaultAccessToken( $accessToken );
-                        
+
                         try {
-                            $response = $fb->get( '/me?fields=email,name, first_name, last_name, gender, link, about, bio, birthday, education, hometown, is_verified, languages, location, website' );
+                            $response = $fb->get( '/me?fields=email,name, first_name, last_name, gender, link, about, bio, birthday, education, hometown, languages, location, website' );
                             $userNode = $response->getGraphUser();
                         }
                         catch( Facebook\Exceptions\FacebookResponseException $e ) {
@@ -137,7 +137,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                             echo 'Facebook SDK returned an error: ' . $e->getMessage();
                             exit;
                         }
-                        
+
                         $user_profile = $this->accessProtected( $userNode, 'items' );
                         if( $user_profile != null ) {
                             $return_user_details->status = 'SUCCESS';
@@ -154,11 +154,11 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                             // just a precaution, check whether the header isset...
                             if( isset( $headers['Location'] ) ) {
                                 $return_user_details->deuimage = $headers['Location']; // string
-                                
-                            } 
+
+                            }
                             else {
                                 $return_user_details->deuimage = false; // nothing there? .. weird, but okay!
-                                
+
                             }
                             $return_user_details->error_message = '';
                         }
@@ -200,7 +200,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                 $this->loginUser( $row->ID );
             }
         }
-        
+
         function twitterLogin() {
             $request = $_REQUEST;
             $site = $this->siteUrl();
@@ -221,7 +221,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                     else {
                         $callback = $callBackUrl . 'apsl_login_id' . '=twitter_check';
                     }
-                    
+
                     $request_token = $twitterObj->getRequestToken( $callback );
                     $_SESSION['oauth_twitter'] = array();
                     /* Save temporary credentials to session. */
@@ -319,7 +319,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                 $this->loginUser( $row->ID );
             }
         }
-        
+
         function GoogleLogin() {
             $post = $_POST;
             $get = $_GET;
@@ -332,14 +332,14 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             $action = $a[1];
             $client_id = $options['apsl_google_settings']['apsl_google_client_id'];
             $client_secret = $options['apsl_google_settings']['apsl_google_client_secret'];
-            
+
             $site_url = site_url() . '/wp-admin';
             $encoded_url = isset( $_GET['redirect_to'] ) ? $_GET['redirect_to'] : $site_url;
             $callback = $callBackUrl . 'apsl_login_id' . '=google_check';
-            
+
             $redirect_uri = $callback;
             $client = new Google_Client;
-            
+
             $client->setClientId( $client_id );
             $client->setClientSecret( $client_secret );
             $client->setRedirectUri( $redirect_uri );
@@ -347,9 +347,9 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             if( isset( $encoded_url ) && $encoded_url != '' ) {
                 $client->setState( base64_encode( "redirect_to=$encoded_url" ) );
             }
-            
+
             $service = new Google_Service_Plus( $client );
-            
+
             if( $action == 'login' ) { // Get identity from user and redirect browser to OpenID Server
                 if( !( isset( $_SESSION['access_token'] ) && $_SESSION['access_token'] ) ) {
                     $authUrl = $client->createAuthUrl();
@@ -369,7 +369,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             }
             elseif( isset( $_SESSION['access_token'] ) && $_SESSION['access_token'] ) {
                 $client->setAccessToken( $_SESSION['access_token'] );
-                
+
                 try {
                     $user = $service->people->get( "me", array() );
                 }
@@ -379,7 +379,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                     echo $ref_object[0]['message'] . " Please notify about this error to the Site Admin.";
                     die();
                 }
-                
+
                 if( !empty( $user ) ) {
                     if( !empty( $user->emails ) ) {
                         $response->email = $user->emails[0]->value;
@@ -395,24 +395,24 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
                         $response->deutype = 'google';
                         $response->status = 'SUCCESS';
                         $response->error_message = '';
-                    } 
+                    }
                     else {
                         $response->status = 'ERROR';
                         $response->error_code = 2;
                         $response->error_message = "INVALID AUTHORIZATION";
                     }
-                } 
+                }
                 else { // Signature Verification Failed
                     $response->status = 'ERROR';
                     $response->error_code = 2;
                     $response->error_message = "INVALID AUTHORIZATION";
                 }
-            } 
+            }
             elseif( $get['openid_mode'] == 'cancel' ) { // User Canceled your Request
                 $response->status = 'ERROR';
                 $response->error_code = 1;
                 $response->error_message = "USER CANCELED REQUEST";
-            } 
+            }
             else { // User failed to login
                 $response->status = 'ERROR';
                 $response->error_code = 3;
@@ -424,13 +424,13 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
         function siteUrl() {
             return site_url();
         }
-        
+
         function callBackUrl() {
             $connection = !empty( $_SERVER['HTTPS'] ) ? 'https://' : 'http://';
             $url = $connection . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"];
             if( strpos( $url, '?' ) === false ) {
                 $url.= '?';
-            } 
+            }
             else {
                 $url.= '&';
             }
@@ -442,19 +442,19 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             $json_response = wp_remote_retrieve_body( $response );
             return $json_response;
         }
-        
+
         function redirect( $redirect ) {
             if( headers_sent() ) { // Use JavaScript to redirect if content has been previously sent (not recommended, but safe)
                 echo '<script language="JavaScript" type="text/javascript">window.location=\'';
                 echo $redirect;
                 echo '\';</script>';
-            } 
+            }
             else { // Default Header Redirect
                 header( 'Location: ' . $redirect );
             }
             exit;
         }
-        
+
         function updateUser( $username, $email ) {
             $row = $this->getUserByUsername( $username );
             if( $row && $email != '' && $row->user_email != $email ) {
@@ -466,18 +466,18 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
         // Redefine user notification function
         // function wp_new_user_notification( $user_id, $plaintext_pass = '' ) {
         //     $user = new WP_User( $user_id );
-            
+
         //     $user_login = stripslashes( $user->user_login );
         //     $user_email = stripslashes( $user->user_email );
-            
+
         //     $message = sprintf( __( 'New user registration on your blog %s:' ), get_option( 'blogname' ) ) . "\r\n\r\n";
         //     $message.= sprintf( __( 'Username: %s' ), $user_login ) . "\r\n\r\n";
         //     $message.= sprintf( __( 'E-mail: %s' ), $user_email ) . "\r\n";
-            
+
         //     @wp_mail( get_option( 'admin_email' ), sprintf( __( '[%s] New User Registration' ), get_option( 'blogname' ) ), $message );
-            
+
         //     if( empty( $plaintext_pass ) )return;
-            
+
         //     $message = __( 'Hi there,' ) . "\r\n\r\n";
         //     $message.= sprintf( __( "Welcome to %s! Here's how to log in:" ), get_option( 'blogname' ) ) . "\r\n\r\n";
         //     $message.= wp_login_url() . "\r\n";
@@ -485,10 +485,10 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
         //     $message.= sprintf( __( 'Password: %s' ), $plaintext_pass ) . "\r\n\r\n";
         //     $message.= sprintf( __( 'If you have any problems, please contact me at %s.' ), get_option( 'admin_email' ) ) . "\r\n\r\n";
         //     $message.= __( 'Adios!' );
-            
+
         //     wp_mail( $user_email, sprintf( __( '[%s] Your username and password' ), get_option( 'blogname' ) ), $message );
         // }
-        
+
         function getUserByMail( $email ) {
             global $wpdb;
             $row = $wpdb->get_row( "SELECT * FROM $wpdb->users WHERE user_email = '$email'" );
@@ -497,7 +497,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             }
             return false;
         }
-        
+
         function getUserByUsername( $username ) {
             global $wpdb;
             $row = $wpdb->get_row( "SELECT * FROM $wpdb->users WHERE user_login = '$username'" );
@@ -506,7 +506,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             }
             return false;
         }
-        
+
         function creatUser( $user_name, $user_email ) {
             $random_password = wp_generate_password( 12, false );
             $user_id = wp_create_user( $user_name, $random_password, $user_email );
@@ -517,7 +517,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             }
             return $user_id;
         }
-        
+
         function set_cookies( $user_id = 0, $remember = true ) {
             if( !function_exists( 'wp_set_auth_cookie' ) ) {
                 return false;
@@ -530,17 +530,17 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             wp_set_current_user( $user_id );
             return true;
         }
-        
+
         function loginUser( $user_id ) {
             $current_url_an = get_permalink();
             $reauth = empty( $_REQUEST['reauth'] ) ? false : true;
             if( $reauth )wp_clear_auth_cookie();
-            
+
             if( isset( $_REQUEST['redirect_to'] ) ) {
                 $redirect_to = $_REQUEST['redirect_to'];
                 // Redirect to https if user wants ssl
                 if( isset( $secure_cookie ) && false !== strpos( $redirect_to, 'wp-admin' ) )$redirect_to = preg_replace( '|^http://|', 'https://', $redirect_to );
-            } 
+            }
             else {
                 $redirect_to = admin_url();
             }
@@ -548,38 +548,38 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             // If cookies are disabled we can't log in even with a valid user+pass
             if( isset( $_POST['testcookie'] ) && empty( $_COOKIE[TEST_COOKIE] ) )$user = new WP_Error( 'test_cookie', __( "<strong>ERROR</strong>: Cookies are blocked or not supported by your browser. You must <a href='http://www.google.com/cookies.html'>enable cookies</a> to use WordPress." ) );
             else $user = wp_signon( '', isset( $secure_cookie ) );
-            
+
             if( !$this->set_cookies( $user_id ) ) {
                 return false;
             }
             $requested_redirect_to = isset( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : site_url();
             $user_login_url = apply_filters( 'login_redirect', $redirect_to, $requested_redirect_to, $user );
-            
+
             $options = get_option( APSL_SETTINGS );
             if( isset( $options['apsl_custom_login_redirect_options'] ) && $options['apsl_custom_login_redirect_options'] != '' ) {
                 if( $options['apsl_custom_login_redirect_options'] == 'home' ) {
                     $user_login_url = home_url();
-                } 
+                }
                 else if( $options['apsl_custom_login_redirect_options'] == 'current_page' ) {
                     if( isset( $_REQUEST['redirect_to'] ) ) {
                         $redirect_to = $_REQUEST['redirect_to'];
                         // Redirect to https if user wants ssl
                         if( isset( $secure_cookie ) && false !== strpos( $redirect_to, 'wp-admin' ) )$user_login_url = preg_replace( '|^http://|', 'https://', $redirect_to );
-                    } 
-                    else {
-                        $user_login_url = home_url();
                     }
-                } 
-                else if( $options['apsl_custom_login_redirect_options'] == 'custom_page' ) {
-                    if( $options['apsl_custom_login_redirect_link'] != '' ) {
-                        $login_page = $options['apsl_custom_login_redirect_link'];
-                        $user_login_url = $login_page;
-                    } 
                     else {
                         $user_login_url = home_url();
                     }
                 }
-            } 
+                else if( $options['apsl_custom_login_redirect_options'] == 'custom_page' ) {
+                    if( $options['apsl_custom_login_redirect_link'] != '' ) {
+                        $login_page = $options['apsl_custom_login_redirect_link'];
+                        $user_login_url = $login_page;
+                    }
+                    else {
+                        $user_login_url = home_url();
+                    }
+                }
+            }
             else {
                 $user_login_url = home_url();
             }
@@ -596,7 +596,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             $pageURL.= "://";
             if( $_SERVER["SERVER_PORT"] != "80" ) {
                 $pageURL.= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-            } 
+            }
             else {
                 $pageURL.= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
             }
@@ -610,7 +610,7 @@ if( !class_exists( 'APSL_Lite_Login_Check_Class' ) ) {
             return $property->getValue( $obj );
         }
     } //termination of a class
-    
+
 } //end of if statement
 
 $apsl_login_check = new APSL_Lite_Login_Check_Class();
